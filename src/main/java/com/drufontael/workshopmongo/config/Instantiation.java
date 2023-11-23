8b86994e6fd5707024a671eabf25b9b539c828cf
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-11-23 14:12:39 -0300

    :sparkles: feat:Criando entity Post com User aninhado

--- src/main/java/com/drufontael/workshopmongo/config/Instantiation.java
+++ src/main/java/com/drufontael/workshopmongo/config/Instantiation.java
@@ -1,24 +1,41 @@
 package com.drufontael.workshopmongo.config;
 
+import com.drufontael.workshopmongo.domain.Post;
 import com.drufontael.workshopmongo.domain.User;
+import com.drufontael.workshopmongo.repository.PostRepository;
 import com.drufontael.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
+
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        repository.saveAll(Arrays.asList(maria,alex,bob));
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018",formatter),"Partiu viagem","Vou viajar " +
+                "para São Paulo. Abraços",maria);
+        Post post2 = new Post(null,LocalDate.parse("21/03/2018",formatter),"Bom dia","Acordei feliz hoje!",
+                maria);
+        postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
