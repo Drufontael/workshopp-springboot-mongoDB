af8947cc2b63cc70b12493a0ad74b8f417db982a
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-11-24 10:02:56 -0300

    :sparkles: feat: Acrescentando comentários aos posts

--- src/main/java/com/drufontael/workshopmongo/config/Instantiation.java
+++ src/main/java/com/drufontael/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.drufontael.workshopmongo.config;
 import com.drufontael.workshopmongo.domain.Post;
 import com.drufontael.workshopmongo.domain.User;
 import com.drufontael.workshopmongo.dto.AuthorDTO;
+import com.drufontael.workshopmongo.dto.CommentDTO;
 import com.drufontael.workshopmongo.repository.PostRepository;
 import com.drufontael.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,14 @@ public class Instantiation implements CommandLineRunner {
                 "para São Paulo. Abraços",new AuthorDTO(maria));
         Post post2 = new Post(null,LocalDate.parse("21/03/2018",formatter),"Bom dia","Acordei feliz hoje!",
                 new AuthorDTO(maria));
+
+        CommentDTO c1=new CommentDTO("Boa viagem mano!",LocalDate.parse("21/03/2018",formatter),new AuthorDTO(alex));
+        CommentDTO c2=new CommentDTO("Aproveite!",LocalDate.parse("22/03/2018",formatter),new AuthorDTO(bob));
+        CommentDTO c3=new CommentDTO("Tenha um ótimo dia!",LocalDate.parse("23/03/2018",formatter),new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
+
         postRepository.saveAll(Arrays.asList(post1,post2));
 
         maria.getPosts().addAll(Arrays.asList(post1,post2));
