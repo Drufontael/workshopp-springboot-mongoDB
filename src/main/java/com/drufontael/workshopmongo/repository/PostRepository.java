b8ac71c56d49ea5466a94c40d264c6c5da97a5ac
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-11-24 12:46:53 -0300

    :recycle: refactor: Consulta simples com @Query

--- src/main/java/com/drufontael/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/drufontael/workshopmongo/repository/PostRepository.java
@@ -2,11 +2,15 @@ package com.drufontael.workshopmongo.repository;
 
 import com.drufontael.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    @Query("{'title': {$regex: ?0, $options:'i'}}")
+    List<Post> searchByTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
